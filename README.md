4f1d011c8ed852f53ee6f4c55505ce21bf8b2949
bzubs <bzubs011@gmail.com>
2026-02-25 22:19:08 +0530

    added least available connection available

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Large-scale Production Grade Implementation of a Reverse Proxy that handles load under pre- configured latency and throughput requirements.
 
 
-Balancing Algorithms available for v1:
+Balancing Algorithms available for v0.1.1:
 1) Round Robin
+2) Least Connections Available
---
+++ leastConn.js
@@ -0,0 +1,55 @@
+import http from 'http';
+
+
+function leastConn(PORT = 3000, backends = [], history = [], index = 0) {
+
+    if (backends.length === 0) {
+        return null;
+    }
+
+    const balancer = http.createServer((req, res) => {
+
+        const backend = backends.reduce((a, b) => {
+            return a.currentRequests < b.currentRequests ? a : b;
+        })
+
+        let cleaned = false;
+
+        function cleanup() {
+            if (cleaned) return;
+            cleaned = true;
+            backend.currentRequests--;
+        }
+
+        const options = {
+            hostname: backend.hostname,
+            port: backend.port,
+            path: req.path,
+            method: req.method,
+            headers: req.headers
+
+        }
+
+        const start = Date.now();
+
+
+        const proxyReq = http.request(options, (proxyRes) => {
+            res.writeHead(proxyRes.statusCode, proxyRes.headers);
+            proxyRes.pipe(res);
+
+
+            res.on('finish', () => {
+                const laten = Date.now() - start;
+
+
+            })
+        })
+
+
+
+
+
+    })
+
+
+}
